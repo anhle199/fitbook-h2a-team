6e019c657064accea9479da8e0cf35df30042b49
Nguyen Hua Hung <63572298+huahungnguyen121@users.noreply.github.com>
2022-04-01 14:52:54 +0700

    FB-1 - Add main flow with navigation drawer

---
+++ fitbook/app/src/main/java/com/h2a/fitbook/ui/gallery/GalleryViewModel.kt
@@ -0,0 +1,13 @@
+package com.h2a.fitbook.ui.gallery
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+
+class GalleryViewModel : ViewModel() {
+
+    private val _text = MutableLiveData<String>().apply {
+        value = "This is gallery Fragment"
+    }
+    val text: LiveData<String> = _text
+}
