deb9780b13b3235f5e1718fdb6a874f6948716d8
Huy Le Minh <leminhhuy.hcmus@gmail.com>
2022-04-21 23:49:42 +0700

    Finish view exercise list (home view) & view exercise detail screens & add some models

--- fitbook/app/src/main/java/com/h2a/fitbook/viewmodels/home/HomeViewModel.kt
+++ fitbook/app/src/main/java/com/h2a/fitbook/viewmodels/home/HomeViewModel.kt
@@ -1,6 +1,32 @@
 package com.h2a.fitbook.viewmodels.home
 
+import android.util.Log
 import androidx.lifecycle.ViewModel
+import com.google.firebase.firestore.FirebaseFirestore
+import com.h2a.fitbook.models.ExerciseListItemModel
 
 class HomeViewModel : ViewModel() {
+    private val firestore by lazy {
+        FirebaseFirestore.getInstance()
+    }
+
+    fun getExerciseList(callback: (Boolean, ArrayList<ExerciseListItemModel>) -> Unit) {
+        firestore.collection("exercises").get().addOnCompleteListener{ task ->
+            if (task.isSuccessful) {
+                val list: ArrayList<ExerciseListItemModel> = arrayListOf()
+                for (document in task.result) {
+                    val item = document.toObject(ExerciseListItemModel::class.java)
+                    item.id = document.id
+                    list.add(item)
+                }
+                callback(task.isSuccessful, list)
+            } else {
+                callback(false, arrayListOf())
+                Log.i("Firebase", task.exception.toString())
+            }
+        }.addOnFailureListener {
+            callback(false, arrayListOf())
+            Log.i("Firebase", it.toString())
+        }
+    }
 }
--- fitbook/app/src/main/java/com/h2a/fitbook/views/activities/home/HomeDetailActivity.kt
+++ fitbook/app/src/main/java/com/h2a/fitbook/views/activities/home/HomeDetailActivity.kt
@@ -3,18 +3,19 @@ package com.h2a.fitbook.views.activities.home
 import android.app.Activity
 import android.content.Intent
 import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
 import androidx.activity.result.ActivityResult
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
 import com.google.android.material.floatingactionbutton.FloatingActionButton
 import com.h2a.fitbook.R
-import com.h2a.fitbook.models.ExerciseModel
 import com.h2a.fitbook.viewmodels.home.ExerciseDetailViewModel
 
 class HomeDetailActivity : AppCompatActivity() {
     private lateinit var createScheduleFab: FloatingActionButton
-    private val exerciseDetailViewModel: ExerciseDetailViewModel by viewModels()
+    private val viewModel: ExerciseDetailViewModel by viewModels()
 
     private val getSecondActivityIntent =
         registerForActivityResult(ActivityResultContracts.StartActivityForResult()) { result: ActivityResult ->
@@ -29,33 +30,25 @@ class HomeDetailActivity : AppCompatActivity() {
 
         // get exercise by id from API
         val exerciseId = intent.getStringExtra("EXERCISE_ID")
-        val steps = arrayListOf<String>()
-        for (i in 1..5) {
-            steps.add("Lorem ipsum dolor sit amet, consectetur adipiscing elit. Id sed vitae diam morbi ut eget dolor. Suspendisse dictum ipsum risus sodales nisi id.")
-        }
-        val exercise = ExerciseModel(
-            "1",
-            "Bài 1",
-            2,
-            50,
-            "https://i.ibb.co/qg4Pjx4/bg-home-list-item-running.png",
-            "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Id sed vitae diam morbi ut eget dolor. Suspendisse dictum ipsum risus sodales nisi id.",
-            steps
-        )
-
-        exerciseDetailViewModel.setThumbnail(exercise.image)
-        exerciseDetailViewModel.setTitle(exercise.title)
-        exerciseDetailViewModel.setDetail("${exercise.duration} Phút | ${exercise.calories} Calo")
-        exerciseDetailViewModel.setDescription(exercise.description)
-        exerciseDetailViewModel.setSteps(exercise.steps)
-
-        createScheduleFab = findViewById(R.id.home_detail_fab_create_schedule)
-        createScheduleFab.setOnClickListener {
-            val intent = Intent(this, HomeRescheduleActivity::class.java)
-            intent.putExtra("EXERCISE_THUMBNAIL", exercise.image)
-            intent.putExtra("EXERCISE_TITLE", exercise.title)
-            intent.putExtra("EXERCISE_DETAIL", "${exercise.duration} Phút | ${exercise.calories} Calo")
-            getSecondActivityIntent.launch(intent)
+        viewModel.setLoading(true)
+        viewModel.getExerciseDetail(exerciseId!!) { isOk, data ->
+            viewModel.setLoading(false)
+            if (!isOk) {
+                Toast.makeText(this, R.string.activity_home_detail_load_error, Toast.LENGTH_SHORT)
+                    .show()
+            } else {
+                createScheduleFab = findViewById(R.id.home_detail_fab_create_schedule)
+                createScheduleFab.setOnClickListener {
+                    val intent = Intent(this, HomeRescheduleActivity::class.java)
+                    intent.putExtra("EXERCISE_THUMBNAIL", data.image)
+                    intent.putExtra("EXERCISE_TITLE", data.name)
+                    intent.putExtra(
+                        "EXERCISE_DETAIL",
+                        "${data.measureDuration / 60} Phút | ${data.measureCalories} Calo"
+                    )
+                    getSecondActivityIntent.launch(intent)
+                }
+            }
         }
     }
 }
